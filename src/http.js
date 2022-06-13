b013cfbef03672aa05066756191c5af9d8a13a89
Boaz Blake <BBlake@goempyrean.com>
2022-06-12 20:21:31 -0500

    update env

--- src/http.js
+++ src/http.js
@@ -1,7 +1,7 @@
 import Task from "data.task"
 import m from "mithril"
 
-const baseUrl = "http://localhost:8000/"
+const baseUrl = process.env.API_URL
 
 const headers = {
   "Content-Type": "application/json",
